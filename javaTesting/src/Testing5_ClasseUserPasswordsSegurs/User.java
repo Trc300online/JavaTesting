ac254f2c83383e794973309e5a6a442e330e170a
Trc300online <186137334+Trc300online@users.noreply.github.com>
2025-04-12 20:03:47 +0200

    Testing 5, vid min 20

---
+++ javaTesting/src/Testing5_ClasseUserPasswordsSegurs/User.java
@@ -0,0 +1,34 @@
+package Testing5_ClasseUserPasswordsSegurs;
+
+public class User {
+
+    private String nom;
+    private String password;  // password in hash mode and salted
+
+    public User() {
+        this("Toni", "secret");
+    }
+
+    public User(String nom, String password) {
+        this.nom = nom;
+        this.password = password;
+    }
+
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
---
+++ javaTesting/src/Testing5_ClasseUserPasswordsSegurs/UserTest.java
@@ -0,0 +1,17 @@
+package Testing5_ClasseUserPasswordsSegurs;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class UserTest {
+
+    @Test
+    public void constructUserTest() {
+        User usuari = new User("Toni","secret");
+
+        assertTrue(usuari != null);
+
+        assertEquals(usuari.getNom(), "Toni");
+        assertEquals(usuari.getPassword(), "secret");
+    }
+}
